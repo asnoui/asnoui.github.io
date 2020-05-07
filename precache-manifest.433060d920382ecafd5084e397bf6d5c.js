7af93830a180328307b28b26ebac1fe0a5bc3f6e
kourrai <karim.ourrai@docaposte.fr>
2020-05-07 20:23:32 +0000

    build

---
+++ precache-manifest.433060d920382ecafd5084e397bf6d5c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4baf2b1f3c847c30ad641f391ec95d3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b38a0a6479b58014958b",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "57335e2f36582262e74d",
+    "url": "/static/js/2.d7d64c9e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d7d64c9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b38a0a6479b58014958b",
+    "url": "/static/js/main.aeeadea0.chunk.js"
+  },
+  {
+    "revision": "6a8eeeca7179c8ebb553",
+    "url": "/static/js/runtime-main.67879012.js"
+  },
+  {
+    "revision": "5b08b9d88d6b7a3ab0def9ec4703f91a",
+    "url": "/static/media/logo.5b08b9d8.svg"
+  }
+]);
